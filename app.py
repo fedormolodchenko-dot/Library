2a4feabdec2fd7d18f18fe98073008c5467b2250
Margo <margo@email.ru>
2025-10-12 23:18:55 +0300

    12-10 repo.py, app.py

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ import db
 import repo
 connect = db.connect_db()
 curs = connect.cursor()
-title, author, genre = input()
+title= input()
+author = input()
+genre = input()
 n = int(input())
-
 repo.add_book(connect, title, author, genre, n)
+repo.remove_book(connect, title, author)
+full_name = input()
+phone = input()
+age = int(input())
+repo.add_reader(connect, full_name, phone, age)
+pr = input()
+repo.remove_reader(connect, pr)
--- repo.py
+++ repo.py
@@ -17,3 +17,40 @@ def add_book(connect, title, author, genre, n=1):
                        VALUES(?,?)""", (n,n))
     cursor.commit()
 
+def remove_book(connect, title, author):
+    cursor = connect.Cursor()
+    cursor.execute("""SELECT id FROM books
+                   WHERE title == ?, author == ?""",(title, author))
+    s = cursor.fetchall()
+    cursor.execute("""SELECT book_id FROM loans
+                   WHERE book_id == ?""",(s))
+    loans = cursor.fetchall()
+    cursor.execute("""SELECT book_id FROM holds
+                   WHERE book_id == ?""",(s))
+    holds = cursor.fetchall()
+    if loans == [] and holds == []:
+        cursor.execute("""DELETE FROM books
+                       WHERE title == ?, author == ?""",(title, author))
+    cursor.commit()
+
+def add_reader(connect, full_name, phone, age):
+    cursor = connect.Cursor()
+    name, surname = full_name.split()
+    pr = name[0] + surname[0] + str(len(name)) + str(len(surname)) + phone[len(phone)- 4:]
+    cursor.execute("""INSERT INTO readers (pr, full_name, phone, age)
+                   VALUES (?, ?, ?, ?)""", (pr, full_name, phone, age))
+    cursor.commit()
+
+def remove_reader(connect, pr):
+    cursor = connect.Cursor()
+    cursor.execute("""SELECT pr FROM loans""")
+    loans = cursor.fetchall()
+    cursor.execute("""SELECT pr FROM holds""")
+    holds = cursor.fetchall()
+    if loans == [] and holds == []:
+        cursor.execute("""DELETE FROM readers 
+                       WHERE pr == ?""", (pr))
+    cursor.commit()
+
+
+
