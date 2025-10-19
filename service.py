3122ba6f83e3b2e45dee293da77df92df4c45bd8
fedor <fedormolodchenko@gmail.com>
2025-10-19 15:15:30 +0300

    == changed to =

--- service.py
+++ service.py
@@ -2,7 +2,7 @@
 def bron(connect, pr, title, author):
     cursor = connect.cursor()
     cursor.execute("""SELECT id, free FROM books
-                      WHERE title == ?, author == ?""", (title , author))
+                      WHERE title = ?, author = ?""", (title , author))
     s = cursor.fetchall()
     
     if not s:
@@ -13,7 +13,7 @@ def bron(connect, pr, title, author):
     free = s[0][1]     
 
     cursor.execute("""SELECT count(book_id) FROM holds
-                   WHERE pr == ?""", (pr))
+                   WHERE pr = ?""", (pr))
     cnt = cursor.fetchall()
 
     if int(free) > 0 and int(cnt) <= 5:
@@ -22,13 +22,13 @@ def bron(connect, pr, title, author):
                        VALUES (?, ?, ?)""", (pr, book_id, date))
         cursor.execute("""UPDATE books
                        SET free = free - 1
-                       WHERE title == ?, author == ?""", (title, author))
+                       WHERE title = ?, author = ?""", (title, author))
     cursor.commit()
         
 def remove_bron(connect, pr, title, author):
     cursor = connect.cursor()
     cursor.execute("""SELECT id FROM books
-                      WHERE title == ?, author == ?""", (title , author))
+                      WHERE title = ?, author = ?""", (title , author))
     
     s = cursor.fetchall()
     if not s: 
@@ -38,16 +38,16 @@ def remove_bron(connect, pr, title, author):
     book_id = s[0][0] 
 
     cursor.execute("""DELETE FROM holds
-                   WHERE pr == ?, book_id == ?""", (pr, book_id))
+                   WHERE pr = ?, book_id = ?""", (pr, book_id))
     cursor.execute("""UPDATE books
                        SET free = free + 1
-                       WHERE title == ?, author == ?""", (title, author))
+                       WHERE title = ?, author = ?""", (title, author))
     cursor.commit()
 
 def take_book(connect, pr, title, author):
     cursor = connect.cursor()
     cursor.execute("""SELECT free, id FROM books
-                   WHERE title == ?, author == ?""", (title , author))
+                   WHERE title = ?, author = ?""", (title , author))
     
     book_result = cursor.fetchall()
     if not book_result:
@@ -57,17 +57,17 @@ def take_book(connect, pr, title, author):
     free = book_result[0][1]
 
     cursor.execute("""SELECT book_id FROM holds
-                   WHERE pr == ?""", (pr))
+                   WHERE pr = ?""", (pr))
     book_id_2 = cursor.fetchall()
     cursor.execute("""SELECT count(book_id)
-                   WHERE pr == ?""", (pr))
+                   WHERE pr = ?""", (pr))
     active_takes = cursor.fetchall()
     if int(active_takes) < 5:
         if int(free) > 0 or (book_id in book_id_2):
             date = "d/m/y"
             cursor.execute("""UPDATE books
                         SET free = free - 1
-                        WHERE title == ?, author == ?""", (title, author))
+                        WHERE title = ?, author = ?""", (title, author))
             cursor.execute("""INSERT INTO loans(pr, book_id, date)
                            VAlUES (?, ?, ?)""", (pr, book_id, date))
     cursor.commit()
@@ -75,7 +75,7 @@ def take_book(connect, pr, title, author):
 def back_book(connect, pr, title, author):
     cursor = connect.cursor()
     cursor.execute("""SELECT free, id FROM books
-                   WHERE title == ?, author == ?""", (title , author))
+                   WHERE title = ?, author = ?""", (title , author))
     
     book_result = cursor.fetchall()
     if not book_result:
@@ -84,10 +84,10 @@ def back_book(connect, pr, title, author):
     book_id = book_result[0][0]
 
     cursor.execute("""DELETE FROM loans
-                   WHERE pr == ?, book_id == ?""", (pr, book_id))
+                   WHERE pr = ?, book_id = ?""", (pr, book_id))
     cursor.execute("""UPDATE books
                    SET free = free + 1
-                   WHERE title == ?, author == ?""", (title, author))
+                   WHERE title = ?, author = ?""", (title, author))
     cursor.commit()
 
 def get_taken_books(connect, pr):
